d043ea8704e0f0d711c6167a649977621554e09a
Alí Rodríguez C <92193598+alirodcor98@users.noreply.github.com>
2021-11-11 14:41:12 -0600

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -3,16 +3,24 @@
 import socket #library to use https://docs.python.org/3/library/socket.html
 from ports import ports_and_services
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 def getOpenPorts(target, portRange)
     openPorts = []
     #Obtener el host con socket
-
-    #hacer un loop sobre el rango de puertos
+    target_ip = socket.gethostbyname(target)
     
-    #abrir el socket determinado
-
-    #verificar si se conecta
-
-    #si se conecta agregarlo al array
+    def port_scan(port):
+    try:
+        s.connect((target_ip, port))
+        return True
+    except:
+        return False
+    
+    #hacer un loop sobre el rango de puertos
+    for port in portRange:
+        if port_scan(port):
+           openPorts.append(port)
+        
     return openPorts
 
